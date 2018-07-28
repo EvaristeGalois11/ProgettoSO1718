04b79b951d1148fd4fbf0d749c3f1714a0b24022
andrea <andreascari8@gmail.com>
2018-07-29 01:08:50 +0200

    Completata inizializzazione dei file di testo

--- src/padre.c
+++ src/padre.c
@@ -3,35 +3,26 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <string.h>
+#include <malloc.h>
 #define PATHSTART "./MAx/MA"
-#define PATHEND ".txt"
+#define NUMBER_OF_SEGMENTS 16
+#define ALLPRIVILEGES 0777
 
 int main(int argc, char *argv[]) {
 
 	struct stat st = {0};
 	if(stat("./MAx",&st) == -1){	//crea directory se non esiste
-		mkdir("./MAx", 0777);
+		mkdir("./MAx", ALLPRIVILEGES);
 	}
-
-	for(int i=1;i<17;i++){		//crea i 16 nomi dei file di testo
-		char path[15]=PATHSTART;
-		int temp=i;
-		if(temp>9){
-			path[8]='1';
-			path[9]=(temp-10)+'0';
-		}else{
-			path[8]=temp+'0';
-		}
-		
-		int fd = open(strcat(path, PATHEND), O_CREAT | O_RDWR, 0777);
+	umask(0000);
+	char *path=(char*)malloc(10*sizeof(char));
+	for(int i=1;i<NUMBER_OF_SEGMENTS + 1;i++){		//crea i 16 nomi dei file di testo
+		sprintf(path,"%s%d", PATHSTART, i);
+		int fd = open(path, O_CREAT | O_RDWR, ALLPRIVILEGES);
+		write(fd,"0",1);
+		close(fd);
 	}
-	/*int fd;
-	fd=open("./MAx/MA1.txt", O_RDWR | O_CREAT, 0777);
-	if (fd==-1){
-		printf("roflxd\n");
-	}*/
+	free(path);
+	path=NULL;
 	return 0;
-	
-
 }
