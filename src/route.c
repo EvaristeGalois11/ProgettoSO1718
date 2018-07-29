d5df3356246b96f5281871277b9246c982d43be9
Claudio Nave <claudionave97@gmail.com>
2018-07-29 15:49:23 +0200

    Ultimata costruzione della rotta di un treno

--- src/route.c
+++ src/route.c
@@ -6,25 +6,41 @@
 
 #define SEGMENT_PREFIX "MA"
 #define STATION_PREFIX "S"
+#define CSV_SEPARATOR ","
 
-static int decodify(char *);
+static Node *buildNode(char *);
 static char *readLine(char *);
 
-Node generateRoute(char *string) {
+Node *generateRoute(char *path) {
+    char *line = readLine(path);
+    char *saveState;
+    char *token = strtok_r(line, CSV_SEPARATOR, &saveState);
+    Node *start = NULL;
+    Node *current;
+    while (token != NULL) {
+        if (start == NULL) {
+            start = buildNode(token);
+            current = start;
+        } else {
+            current -> next = buildNode(token);
+            current = current -> next;
+        }
+        token = strtok_r(NULL, CSV_SEPARATOR, &saveState);
+    }
+    free(line);
+    return start;
 }
 
 char *readLine(char *path) {
     FILE * file = fopen(path, "r");
-
-    int CUR_MAX = 20;
+    int CURR_MAX = 40;
     char ch = getc(file);
-    char *buffer = (char*) malloc(sizeof(char) * CUR_MAX);
+    char *buffer = malloc(sizeof(char) * CURR_MAX);
     int length = 0;
-
     while ((ch != '\n') && (ch != EOF)) {
-        if(length == CUR_MAX) {
-            CUR_MAX *= 2;
-            buffer = realloc(buffer, CUR_MAX);
+        if(length == CURR_MAX) {
+            CURR_MAX *= 2;
+            buffer = realloc(buffer, CURR_MAX);
         }
         if (ch != ' ') {
             buffer[length] = ch;
@@ -32,25 +48,38 @@ char *readLine(char *path) {
         }
         ch = getc(file);
     }
+    if (length == CURR_MAX) {
+        buffer = realloc(buffer, ++CURR_MAX);
+    }
     buffer[length] = '\0';
     return buffer;
 }
 
 
-int decodify(char *name) {
-    int id = 0;
+Node *buildNode(char *name) {
+    Node *node = malloc(sizeof(Node));
+    node -> next = NULL;
     if (strstr(name , SEGMENT_PREFIX) != NULL) {
-        sscanf(name, SEGMENT_PREFIX"%d", &id);
+        sscanf(name, SEGMENT_PREFIX"%d", &(node -> id));
     } else if (strstr(name , STATION_PREFIX) != NULL) {
-        sscanf(name, STATION_PREFIX"%d", &id);
+        sscanf(name, STATION_PREFIX"%d", &(node -> id));
+        node -> id *= -1;
     } else {
         fprintf(stderr, "%s is not a valid string!\n", name);
     }
-    return id;	
+    return node;	
 }
 
-int main(void) { // solo per testing
-    printf("%s", readLine("../resources/routes/T1"));
+int main(void) { // XXX solo per testing
+    Node *temp = generateRoute("../resources/routes/T1");
+    
+    while (temp != NULL) {
+        printf("id = %d\n", temp -> id);
+        temp = temp -> next;
+    }
+    //char *temp = readLine("../resources/routes/T1");
+    //printf("%s | %d\n", temp, strlen(temp));
+    //free(temp);
     //printf("%d\n", decodify("MA2"));
     //printf("%d\n", decodify("S90293"));
     //printf("%d\n", decodify("pippo"));
