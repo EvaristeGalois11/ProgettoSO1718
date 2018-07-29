d3614277b280c181de29754ec9dfb5ecf2c19fe0
andrea <andreascari8@gmail.com>
2018-07-29 17:29:05 +0200

    Implementata la creazione dei processi figli

--- src/padre.c
+++ src/padre.c
@@ -4,25 +4,43 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <malloc.h>
-#define PATHSTART "./MAx/MA"
+#include <string.h>
+#include <stdlib.h>
+#define PATH_SEGMENT_DIR "../resources/MAx"
+#define PATH_SEGMENT_FILE "/MA"
+#define PATH_TRAIN_EXE "../bin/train.out"
 #define NUMBER_OF_SEGMENTS 16
-#define ALLPRIVILEGES 0777
+#define ALL_PRIVILEGES 0777
+#define NUMBER_OF_TRAINS 5
 
 int main(int argc, char *argv[]) {
 
 	struct stat st = {0};
-	if(stat("./MAx",&st) == -1){	//crea directory se non esiste
-		mkdir("./MAx", ALLPRIVILEGES);
+	if(stat(PATH_SEGMENT_DIR,&st) == -1){	//crea directory se non esiste
+		mkdir(PATH_SEGMENT_DIR, ALL_PRIVILEGES);
 	}
 	umask(0000);
 	char *path=(char*)malloc(10*sizeof(char));
-	for(int i=1;i<NUMBER_OF_SEGMENTS + 1;i++){		//crea i 16 nomi dei file di testo
-		sprintf(path,"%s%d", PATHSTART, i);
-		int fd = open(path, O_CREAT | O_RDWR, ALLPRIVILEGES);
+	for(int i=1;i<NUMBER_OF_SEGMENTS + 1;i++){	//crea i 16 nomi dei file di testo
+		sprintf(path,"%s%s%d", PATH_SEGMENT_DIR, PATH_SEGMENT_FILE, i);
+		int fd = open(path, O_CREAT | O_RDWR, ALL_PRIVILEGES);
 		write(fd,"0",1);
 		close(fd);
 	}
 	free(path);
 	path=NULL;
+
+	pid_t pids[NUMBER_OF_TRAINS];
+	for(int i=0;i<NUMBER_OF_TRAINS;i++){
+		if((pids[i] = fork()) == 0){	//sono un figlio
+			//exe
+			char idTrain[12];
+			sprintf(idTrain,"%d",i);
+			execl(PATH_TRAIN_EXE,PATH_TRAIN_EXE,idTrain,argv[1],NULL);
+			}
+	}
+	//questa parte di codice viene eseguita subito dal padre
+	int rofl;
+	scanf("%d",&rofl);
 	return 0;
 }
