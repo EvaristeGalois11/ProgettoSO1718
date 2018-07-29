7d1c09205f14a04f01452cc515423c28fa39ccca
Claudio Nave <claudionave97@gmail.com>
2018-07-29 19:10:58 +0200

    Piccole correzioni padre

--- src/padre.c
+++ src/padre.c
@@ -3,27 +3,26 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <malloc.h>
 #include <string.h>
 #include <stdlib.h>
+
 #define PATH_SEGMENT_DIR "../resources/MAx"
 #define PATH_SEGMENT_FILE "/MA"
-#define PATH_TRAIN_EXE "../bin/train.out"
+#define PATH_TRAIN_EXE "../bin/train"
 #define NUMBER_OF_SEGMENTS 16
-#define ALL_PRIVILEGES 0777
 #define NUMBER_OF_TRAINS 5
 
 int main(int argc, char *argv[]) {
 
 	struct stat st = {0};
 	if(stat(PATH_SEGMENT_DIR,&st) == -1){	//crea directory se non esiste
-		mkdir(PATH_SEGMENT_DIR, ALL_PRIVILEGES);
+		mkdir(PATH_SEGMENT_DIR, 0777);
 	}
 	umask(0000);
 	char *path=(char*)malloc(10*sizeof(char));
 	for(int i=1;i<NUMBER_OF_SEGMENTS + 1;i++){	//crea i 16 nomi dei file di testo
 		sprintf(path,"%s%s%d", PATH_SEGMENT_DIR, PATH_SEGMENT_FILE, i);
-		int fd = open(path, O_CREAT | O_RDWR, ALL_PRIVILEGES);
+		int fd = open(path, O_CREAT | O_RDWR, 0777);
 		write(fd,"0",1);
 		close(fd);
 	}
@@ -37,7 +36,7 @@ int main(int argc, char *argv[]) {
 			char idTrain[12];
 			sprintf(idTrain,"%d",i);
 			execl(PATH_TRAIN_EXE,PATH_TRAIN_EXE,idTrain,argv[1],NULL);
-			}
+		}
 	}
 	//questa parte di codice viene eseguita subito dal padre
 	int rofl;
