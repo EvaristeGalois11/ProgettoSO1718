a01655303261ded3595171a4dbcf40dabb699bf7
andrea <andreascari8@gmail.com>
2018-07-30 23:32:14 +0200

    Formattazione stilistica di ertms.c

--- src/ertms.c
+++ src/ertms.c
@@ -19,7 +19,7 @@
 #define	ETCS2 "ETCS2"
 #define RBC	"RBC"
 
-typedef enum {other,etcs,rbc} launchmode;
+typedef enum {other, etcs, rbc} launchmode;
 
 static void createDirIfNotExist(char *);
 static void createMAxFiles(int);
@@ -33,16 +33,16 @@ static int checkLaunchMode(int, char*[]);
 static char *exeDirPath;
 
 int main(int argc, char *argv[]) {
-	launchmode launchMode=checkLaunchMode(argc,argv);
-	if (launchMode==other){
+	launchmode launchMode = checkLaunchMode(argc, argv);
+	if (launchMode == other) {
 		printf("selezionare un modo per lanciare il programma ETCS1 | ETCS2 | ETCS2 RBC\n");
 		return 0;
 	}
-	else if (launchMode==rbc){
+	else if (launchMode == rbc) {
 		printf("stiamo avviando il server Kappa :)\n");
 		return 0;
 	}
-	exeDirPath=truncExeName(getExePath(EXE_INFO_PATH));
+	exeDirPath = truncExeName(getExePath(EXE_INFO_PATH));
 	mode_t previousMask = umask(0000);
 	createDirIfNotExist(MAX_DIR_PATH);
 	createMAxFiles(NUMBER_OF_MA);
@@ -53,47 +53,47 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-int checkLaunchMode(int argc, char *argv[]){
-	if (argc==2 && (strcmp(argv[1],ETCS1)==0||strcmp(argv[1],ETCS2)==0))
+int checkLaunchMode(int argc, char *argv[]) {
+	if (argc == 2 && (strcmp(argv[1], ETCS1) == 0 || strcmp(argv[1], ETCS2) == 0))
 		return 1;
-	else if (argc==3 && strcmp(argv[1],ETCS2)==0 && strcmp(argv[2],RBC)==0)
+	else if (argc == 3 && strcmp(argv[1], ETCS2) == 0 && strcmp(argv[2], RBC) == 0)
 		return 2;
 	return 0;
 }
 
-char *getExePath(char *filename){
-	int size=10;
-	char *buffer=NULL;
-	while(1){
-		buffer= realloc(buffer, size);
-		int nchars= readlink (filename, buffer, size);
-		if(nchars<0){ //errore se nchars=-1
+char *getExePath(char *filename) {
+	int size = 10;
+	char *buffer = NULL;
+	while (1) {
+		buffer = realloc(buffer, size);
+		int nchars = readlink (filename, buffer, size);
+		if (nchars < 0) { //errore se nchars=-1
 			free(buffer);
 			return NULL;
 		}
-		else if(nchars < size)return buffer;
-		else size*=2; //se nchars==size il filename e' stato troncato e ha bisogno di più spazio
+		else if (nchars < size)return buffer;
+		else size *= 2; //se nchars==size il filename e' stato troncato e ha bisogno di più spazio
 	}
 }
 
-char *truncExeName(char *exePath){
-	int len=strlen(exePath);
-	while(exePath[len]!='/' && len>=0){
+char *truncExeName(char *exePath) {
+	int len = strlen(exePath);
+	while (exePath[len] != '/' && len >= 0) {
 		len--;
-		}
-	if(len==0)return exePath; //errore nel path, non modificare
-	len+=1;	//len+1 per il terminatore
-	exePath=realloc(exePath,len);
-	exePath[len]='\0';
+	}
+	if (len == 0)return exePath; //errore nel path, non modificare
+	len += 1;	//len+1 per il terminatore
+	exePath = realloc(exePath, len);
+	exePath[len] = '\0';
 	return exePath;
 }
 
 void createDirIfNotExist(char *dir) {
 	struct stat st;
-	const unsigned int len=strlen(exeDirPath)+strlen(dir)+1;
+	const unsigned int len = strlen(exeDirPath) + strlen(dir) + 1;
 	char pathExe[len];
-	strcpy(pathExe,exeDirPath);
-	strcat(pathExe,dir);
+	strcpy(pathExe, exeDirPath);
+	strcat(pathExe, dir);
 	if (stat(pathExe, &st) == -1) {
 		mkdir(pathExe, 0777);
 	}
@@ -104,7 +104,7 @@ void createMAxFiles(int num) {
 	int maxPathLength = strlen(exeDirPath) + strlen(MAX_DIR_PATH) + strlen(MA_FILE_PREFIX) + maxDigits + 1;
 	char path[maxPathLength];
 	for (int i = 1; i < num + 1; i++) {
-		sprintf(path, "%s%s%s%d",exeDirPath, MAX_DIR_PATH, MA_FILE_PREFIX, i);
+		sprintf(path, "%s%s%s%d", exeDirPath, MAX_DIR_PATH, MA_FILE_PREFIX, i);
 		int fd = open(path, O_CREAT | O_RDWR | O_TRUNC, 0777);
 		write(fd, "0", 1);
 		close(fd);
@@ -121,9 +121,9 @@ pid_t *startTrains(int num, char *mode) {
 	char id[maxDigits + 1];
 	for (int i = 0; i < num; i++) {
 		if ((pids[i] = fork()) == 0) {	//sono un figlio (di troia)
-			const unsigned int len=strlen(exeDirPath)+strlen(TRAIN_PROCESS_NAME)+1;
+			const unsigned int len = strlen(exeDirPath) + strlen(TRAIN_PROCESS_NAME) + 1;
 			char pathExe[len];
-			sprintf(pathExe,"%s%s",exeDirPath,TRAIN_PROCESS_NAME);
+			sprintf(pathExe, "%s%s", exeDirPath, TRAIN_PROCESS_NAME);
 			sprintf(id, "%d", i);
 			execl(pathExe, pathExe, id, mode, NULL);
 			perror("Child");
