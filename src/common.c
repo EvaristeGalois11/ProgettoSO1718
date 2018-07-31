c7f732672ad3c3e16a3141918d1c835a5be00250
Claudio Nave <claudionave97@gmail.com>
2018-07-31 19:52:30 +0200

    Fattorizzato il calcolo della lunghezza dei path

--- src/common.c
+++ src/common.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdarg.h>
 #include "common.h"
 
 char *exeDirPath;
+
+static int getGenericPathLength(int, ...);
 
 int countDigits(int n) {
 	if (n == 0) {
@@ -33,17 +36,28 @@ char *truncExeName(char *exePath) {
 }
 
 int getPathMAxFileLength(int id) {
-	int digits = countDigits(id);
-	return strlen(exeDirPath) + strlen(MAX_DIR_PATH) + strlen(MA_FILE_PREFIX) + digits + 1;
+	return getGenericPathLength(id, exeDirPath, MAX_DIR_PATH, MA_FILE_PREFIX, NULL);
+}
+
+int getPathRouteFileLength(int id) {
+	return getGenericPathLength(id, exeDirPath, ROUTE_FILE_PREFIX, ROUTE_FILE_PREFIX, NULL);
+}
+
+int getGenericPathLength(int num, ...) {
+	va_list argp;
+	va_start(argp, num);
+	int result;
+	char *string = va_arg(argp, char *);
+	while (string != NULL) {
+		string = va_arg(argp, char *);
+		result += strlen(string);
+	}
+	va_end(argp);
+	return result;
 }
 
 void formatPathMAxFile(char **path, int id) {
 	sprintf(*path, "%s%s%s%d", exeDirPath, MAX_DIR_PATH, MA_FILE_PREFIX, id);
-}
-
-int getPathRouteFileLength(int id) {
-	int digits = countDigits(id);
-	return strlen(exeDirPath) + strlen(ROUTES_DIR_PATH) + strlen(ROUTE_FILE_PREFIX) + digits + 1;
 }
 
 void formatPathRouteFile(char **path, int id) {
