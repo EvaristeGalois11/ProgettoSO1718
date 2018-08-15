c24791ccf7a3647e049837c581ad0f790413ebd0
Claudio Nave <claudionave97@gmail.com>
2018-08-15 19:40:34 +0200

    Escluso il docx dal tarball da consegnare

--- compress.sh
+++ compress.sh
@@ -2,4 +2,4 @@
 #Comprimere solo il necessario per la consegna
 
 make clean
-tar -czvf PROGETTO_SO_1718.tar.gz --exclude=.gitkeep Makefile src bin doc resources/routes
+tar -czvf PROGETTO_SO_1718.tar.gz --exclude=.gitkeep --exclude=*.docx Makefile src bin doc resources/routes
